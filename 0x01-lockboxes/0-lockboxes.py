9be621c7fcc5292779e7be80cae01d56f141e413
Ezejanu <Ezejanu_nno@ymail.com>
2024-02-15 09:15:18 +0000

    updated the function to include checks for sme edge cases

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,9 +3,13 @@
 
 
 def canUnlockAll(boxes):
-    '''Set to keep track of opened boxes'''
+    '''a method that determines if all the boxes can be opened'''
+    
+    if not boxes:
+        return False
 
     opened_boxes = set()
+    '''Set to keep track of opened boxes'''
 
     def dfs(box):
         '''a recursive function to perform depth-first search(DFS)
@@ -14,7 +18,9 @@ def canUnlockAll(boxes):
         opened_boxes.add(box)
 
         for key in boxes[box]:
-            if key not in opened_boxes and key < len(boxes):
+            ''' If the key corresponds to an unopened box and is within
+            valid range, recursively open that box'''
+            if key not in opened_boxes and 0 <= key < len(boxes):
                 dfs(key)
 
     dfs(0)
